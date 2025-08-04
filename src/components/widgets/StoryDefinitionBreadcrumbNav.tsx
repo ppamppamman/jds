fff0945376500f45aefb0aa8d0274a247f3cd1d5
ppamppamman <ppamppamman@gmail.com>
2025-08-05 08:59:19 +0900

    feat: Nav 위젯 컴포넌트 스펙 변경 및 공통화

---
+++ src/components/widgets/StoryDefinitionBreadcrumbNav.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import React, { PropsWithChildren } from "react"
+
+type Props = {
+  current: string
+}
+
+export const StoryDefinitionBreadcrumbNav = ({ current, children }: PropsWithChildren<Props>) => {
+  return (
+    <nav className="mb-8">
+      <div className="flex items-center gap-2 typography-body2 text-semantic-text-secondary">
+        {React.Children.map(children, (child) => (
+          <>
+            {child}
+            <span>/</span>
+          </>
+        ))}
+        <span>/</span>
+        <span className="text-semantic-primary">{current}</span>
+      </div>
+    </nav>
+  )
+}
