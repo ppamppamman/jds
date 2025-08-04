5ebedb82e5413cfb74c6f2bbe7e81ea84922cbad
ppamppamman <ppamppamman@gmail.com>
2025-08-04 16:24:40 +0900

    feat: 버튼 기본 밸류 변경 default -> primary / md, outline -> tertiary

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,37 +5,37 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-offset-2 hover:cursor-pointer",
   {
     variants: {
       variant: {
-        default:
-          "bg-semantic-primary text-semantic-text-white shadow-sm hover:bg-palette-green-600 focus-visible:ring-palette-green-300 disabled:bg-palette-gray-300 disabled:text-palette-gray-500",
-        destructive:
-          "bg-semantic-error text-semantic-text-white shadow-sm hover:bg-palette-red-600 focus-visible:ring-palette-red-300 disabled:bg-palette-gray-300 disabled:text-palette-gray-500",
-        outline:
-          "border border-semantic-border bg-semantic-background text-semantic-text-primary shadow-sm hover:bg-palette-gray-200 hover:text-semantic-text-primary focus-visible:ring-palette-gray-400 disabled:border-palette-gray-300 disabled:bg-transparent disabled:text-palette-gray-400",
+        primary:
+          "bg-semantic-primary text-semantic-text-white shadow-sm hover:bg-palette-green-600 focus-visible:ring-palette-green-300 disabled:bg-palette-gray-500 disabled:text-palette-gray-700",
         secondary:
-          "bg-semantic-secondary text-semantic-text-white shadow-sm hover:bg-palette-gray-600 focus-visible:ring-palette-gray-400 disabled:bg-palette-gray-300 disabled:text-palette-gray-500",
+          "bg-semantic-secondary text-semantic-text-white shadow-sm hover:bg-palette-gray-600 focus-visible:ring-palette-gray-400 disabled:bg-palette-gray-500 disabled:text-palette-gray-700",
+        tertiary:
+          "border border-semantic-border bg-semantic-background text-semantic-text-primary shadow-sm hover:bg-palette-gray-200 hover:text-semantic-text-primary focus-visible:ring-palette-gray-400 disabled:border-palette-gray-500 disabled:bg-transparent disabled:text-palette-gray-600",
         ghost:
-          "text-semantic-text-primary hover:bg-palette-gray-200 hover:text-semantic-text-primary focus-visible:ring-palette-gray-400 disabled:text-palette-gray-400",
+          "text-semantic-text-primary hover:bg-palette-gray-200 hover:text-semantic-text-primary focus-visible:ring-palette-gray-400 disabled:text-palette-gray-600",
+        danger:
+          "bg-semantic-error text-semantic-text-white shadow-sm hover:bg-palette-red-600 focus-visible:ring-palette-red-300 disabled:bg-palette-gray-500 disabled:text-palette-gray-700",
         link: 
-          "text-semantic-primary underline-offset-4 hover:underline focus-visible:ring-palette-green-300 disabled:text-palette-gray-400 disabled:no-underline",
+          "text-palette-blue-500 underline-offset-4 hover:underline focus-visible:ring-palette-green-300 disabled:text-palette-gray-600 disabled:no-underline",
         success:
-          "bg-semantic-success text-semantic-text-white shadow-sm hover:bg-palette-blue-600 focus-visible:ring-palette-blue-300 disabled:bg-palette-gray-300 disabled:text-palette-gray-500",
+          "bg-semantic-success text-semantic-text-white shadow-sm hover:bg-palette-blue-600 focus-visible:ring-palette-blue-300 disabled:bg-palette-gray-500 disabled:text-palette-gray-700",
         warning:
-          "bg-semantic-warning text-semantic-text-black shadow-sm hover:bg-palette-yellow-600 focus-visible:ring-palette-yellow-300 disabled:bg-palette-gray-300 disabled:text-palette-gray-500",
+          "bg-semantic-warning text-semantic-text-black shadow-sm hover:bg-palette-yellow-600 focus-visible:ring-palette-yellow-300 disabled:bg-palette-gray-500 disabled:text-palette-gray-700",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        md: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: "primary",
+      size: "md",
     },
   }
 )
@@ -51,7 +51,6 @@ function Button({
     asChild?: boolean
   }) {
   const Comp = asChild ? Slot : "button"
-
   return (
     <Comp
       data-slot="button"
