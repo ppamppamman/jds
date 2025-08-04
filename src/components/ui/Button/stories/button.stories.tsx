d22dafef41d279f3b7aaff3138c04e3d9d7afb4e
ppamppamman <ppamppamman@gmail.com>
2025-08-04 16:42:41 +0900

    chore: 버튼 컴포넌트와 스토리북 문서 디렉토리 변경

--- src/components/ui/Button/stories/button.stories.tsx
+++ src/components/ui/Button/stories/button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/Button/Button'
 import { Heart, Download, ChevronRight } from 'lucide-react'
 
 const meta = {
